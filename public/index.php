c7f943ee75e44a032c251a952ef00d13d9882c1a
prohladno <program.lera@gmail.com>
2023-05-18 20:24:35 +0200

    created Class Registry

--- public/index.php
+++ public/index.php
@@ -1,2 +1,6 @@
 <?php
 require_once dirname(__DIR__) . '/config/init.php';
+require_once LIBS . '/functions.php';
+
+new \ishop\App();
+debug(\ishop\App::$app->getProperties());
---
+++ vendor/ishop/core/Registry.php
@@ -0,0 +1,24 @@
+<?php
+namespace ishop;
+
+class Registry{
+    use TSingletone;
+
+    protected static $properties = [];
+    
+    public static function setProperty($name, $value){
+        self::$properties[$name] = $value;
+    }
+
+    public function getProoerty($name){
+        if(isset(self::$properties[$name])){
+            return self::$properties[$name];
+        }
+        return null;
+    }
+
+    public function getProperties(){
+        return self::$properties;
+    }
+
+}
